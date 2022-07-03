9be7c78981fc7dc9be15877611876bc22ca3c40d
Aziz Kurbanov <krbnv.z@gmail.com>
2022-07-03 22:41:26 +0600

    *

--- stories-blog/utils/constants.ts
+++ stories-blog/utils/constants.ts
@@ -1,2 +1,2 @@
-const dev = process.env.NODE_ENV !== 'production'
-export const API_URL = dev ? 'http://localhost:3000' : process.env.NODE_ENV
+
+export const API_URL = process.env.PORT || "http://localhost:3000";
